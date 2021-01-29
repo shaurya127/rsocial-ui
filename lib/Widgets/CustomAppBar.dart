709e7d61a89ed19de8c6a919e7bf6cd6a8e3bdbc
Dhruv <dhruv@rsocial.in>
2021-01-29 19:35:53 +0530

    Added total connection and amount to profile pic, and corrected platform_post_tile

--- lib/Widgets/CustomAppBar.dart
+++ lib/Widgets/CustomAppBar.dart
@@ -5,7 +5,7 @@ import 'package:rsocial2/Screens/profile_page.dart';
 import 'package:rsocial2/auth.dart';
 import '../constants.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
+import 'package:rsocial2/functions.dart';
 import '../user.dart';
 
 AppBar customAppBar(context, String title, String lollarAmount, String photoUrl,
@@ -22,29 +22,6 @@ AppBar customAppBar(context, String title, String lollarAmount, String photoUrl,
         ),
       ),
     );
-  }
-
-  String formatNumber(int a) {
-    String res = a.toString();
-
-    if (a < 10000) return res;
-
-    int num = res.length;
-
-    // res = (a/1000).floor().toString() + "," + (a%1000).toString();
-
-    if (num % 2 == 0) {
-      for (int i = 1; i < num; i = i + 2) {
-        res = res.substring(0, i) + "," + res.substring(i);
-        i++;
-      }
-    } else {
-      for (int i = 2; i < num; i = i + 2) {
-        res = res.substring(0, i) + "," + res.substring(i);
-        i++;
-      }
-    }
-    return res;
   }
 
   return AppBar(
