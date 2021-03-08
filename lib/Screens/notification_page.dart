044e2d68c98f39429ed65652b489ca3272777d49
unknown <rishabh8741@gmail.com>
2021-03-08 19:51:31 +0530

    notification page changed

--- lib/Screens/notification_page.dart
+++ lib/Screens/notification_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../contants/constants.dart';
-
+import 'package:rsocial2/Widgets/Notification_tile.dart';
 class NotificationPage extends StatefulWidget {
   @override
   _NotificationPageState createState() => _NotificationPageState();
@@ -47,9 +47,20 @@ class _NotificationPageState extends State<NotificationPage>
         children: <Widget>[
           Container(
             color: Colors.grey.withOpacity(0.1),
+         child: Padding(
+           padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+           child: ListView(
+            children: <Widget>[
+              NotificationTile(name: "aisha",notification: " ewfaw",),
+              NotificationTile(name: "aisha",notification: " rgaerg",),
+              NotificationTile(name: "aisha",notification: " agfawegf",),
+              NotificationTile(name: "aisha",notification: "qga3g34",),
+            ],
+           ),
+         ),
           ),
           Container(
-            color: Colors.grey.withOpacity(0.1),
+            color: Colors.redAccent.withOpacity(0.1),
           ),
         ],
         controller: _tabController2,
